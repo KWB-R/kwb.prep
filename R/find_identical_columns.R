4a1e17a6337964cbb9d55ba223c8ee7a0422cd09
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-11-21 09:18:39 +0100

    Improve find_identical_columns()
    - Use helper function record()
    - Make sure that result is a data frame with
      columns "group", "column_a", "column_b"
    - Add general utility function names_which()

--- R/find_identical_columns.R
+++ R/find_identical_columns.R
@@ -1,33 +1,29 @@
 # find_identical_columns -------------------------------------------------------
 find_identical_columns <- function(a, b)
 {
-  names_a <- names(a)
-  names_b <- names(b)
-
+  record <- function(a, b) {
+    length.out <- max(length(a), length(b))
+    xt <- function(x) kwb.utils::enlargeVector(x, length.out)
+    kwb.utils::noFactorDataFrame(column_a = xt(a), column_b = xt(b))
+  }
+  
   seen <- rep(FALSE, length(a))
 
-  identicals <- lapply(unname(a), function(x) {
+  result <- lapply(unname(a), function(x) {
 
     in_a <- sapply(a, identical, x) & ! seen
     in_b <- sapply(b, identical, x)
-
+  
     seen[in_a] <<- TRUE
 
     if (! any(in_a) || ! any(in_b)) {
-      return()
+      record(character(), character())
+    } else {
+      record(names_which(in_a), names_which(in_b))
     }
-
-    length.out <- max(sum(in_a), sum(in_b))
-    
-    enlarge <- function(x) kwb.utils::enlargeVector(x, length.out)
-
-    kwb.utils::noFactorDataFrame(
-      column_a = enlarge(names_a[in_a]),
-      column_b = enlarge(names_b[in_b])
-    )
   })
 
-  kwb.utils::excludeNULL(identicals, dbg = FALSE) %>%
-    kwb.utils::rbindAll(nameColumn = "group") %>%
-    kwb.utils::moveColumnsToFront("group")
+  kwb.utils::moveColumnsToFront(columns = "group", kwb.utils::rbindAll(
+    result, nameColumn = "group"
+  ))
 }
