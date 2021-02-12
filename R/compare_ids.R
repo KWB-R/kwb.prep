a26d7ef0c702067ad62d0ba3ca9b44d7c9e8acee
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-02-12 11:46:48 +0100

    Simplify compare_ids(), return counts

--- R/compare_ids.R
+++ R/compare_ids.R
@@ -1,26 +1,26 @@
 # compare_ids ------------------------------------------------------------------
-compare_ids <- function(old_ids, new_ids) {
+compare_ids <- function(old, new) {
 
-  common_ids <- intersect(old_ids, new_ids)
+  common <- intersect(old, new)
 
-  added_ids <- setdiff(new_ids, common_ids)
-  removed_ids <- setdiff(old_ids, common_ids)
+  added <- setdiff(new, common)
+  removed <- setdiff(old, common)
 
-  stopifnot(identical(added_ids, new_ids[! new_ids %in% old_ids]))
-  stopifnot(identical(removed_ids, old_ids[! old_ids %in% new_ids]))
+  stopifnot(identical(added, new[! new %in% old]))
+  stopifnot(identical(removed, old[! old %in% new]))
 
-  kwb.utils::printIf(TRUE, length(old_ids))
-  kwb.utils::printIf(TRUE, length(new_ids))
-  kwb.utils::printIf(TRUE, length(common_ids))
-  kwb.utils::printIf(TRUE, length(removed_ids))
-  kwb.utils::printIf(TRUE, length(added_ids))
-
-  kwb.utils::printIf(TRUE, utils::head(sort(removed_ids), 20L))
-  kwb.utils::printIf(TRUE, utils::head(sort(added_ids), 20L))
-
-  list(
-    added_ids = added_ids,
-    removed_ids = removed_ids,
-    common_ids = common_ids
+  result <- list(
+    n_old = length(old),
+    n_new = length(new),
+    n_added = length(added),
+    n_removed = length(removed),
+    n_common = length(common),
+    added = added, 
+    removed = removed, 
+    common = common
   )
+  
+  utils::str(result)
+  
+  result
 }
