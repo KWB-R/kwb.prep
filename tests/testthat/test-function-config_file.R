9fb8ec4ee6a32e7f5e95a15db4fb5a80b0136d2a
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-02-14 06:25:00 +0100

    Look for config file in the package
    otherwise the default path does not exist on
    the machines running the continuous integration

--- tests/testthat/test-function-config_file.R
+++ tests/testthat/test-function-config_file.R
@@ -1,6 +1,6 @@
 test_that("config_file() works", {
 
-  result <- kwb.prep:::config_file()
+  result <- kwb.prep:::config_file(in_package = TRUE)
   
   expect_is(result, "character")
   expect_length(result, 1L)
