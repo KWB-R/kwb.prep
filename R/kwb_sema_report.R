4f0947a6cbe850260342632b7b499f9cafa04a61
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-02-22 18:28:58 +0100

    Handle the case of an empty data frame

--- R/kwb_sema_report.R
+++ R/kwb_sema_report.R
@@ -3,17 +3,39 @@
 # fieldSummary -----------------------------------------------------------------
 fieldSummary <- function(x, groupBy, lengthColumn = "", na = "Unknown")
 {
+  #column=groupBy[1L];lengthColumn=lengthColumn2;na="Unknown"
+  if (nrow(x) == 0L) {
+    
+    message(
+      "The data frame given to fieldSummary() has no rows!"
+    )
+    
+    return(do.call(data.frame, args = c(as.list(x[groupBy]), list(
+      Count = integer(), Percentage = numeric()
+    ))))
+  }
+  
   for (column in groupBy) {
+    
     values <- kwb.utils::selectColumns(x, column)
     levels(values) <- c(levels(values), na)
     x[[column]] <- kwb.utils::defaultIfNA(values, na)
   }
   
-  y <- stats::aggregate(kwb.utils::toFormula("Count", groupBy), cbind(x, Count = 1), length)
+  y <- stats::aggregate(
+    kwb.utils::toFormula("Count", groupBy), 
+    data = cbind(x, Count = 1), 
+    FUN = length
+  )
   
   if (lengthColumn != "") {
     
-    y1 <- stats::aggregate(kwb.utils::toFormula(lengthColumn, groupBy), x, sum, na.rm = TRUE)
+    y1 <- stats::aggregate(
+      kwb.utils::toFormula(lengthColumn, groupBy), 
+      data = x, 
+      FUN = sum, 
+      na.rm = TRUE
+    )
     
     y <- merge(y, y1, by = groupBy)
   }
