a4009e53a64d30104f63e2637c52fa363454583e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2021-03-08 20:15:57 +0100

    Move function definition to where it is used

--- R/applyFilterCriteria.R
+++ R/applyFilterCriteria.R
@@ -140,3 +140,48 @@ keepOrGoSummary <- function(i, lengthColumn, x.base, y.base, criteriaNames)
   
   kwb.utils::removeColumns(out, conditionID)
 }
+
+# fieldSummary -----------------------------------------------------------------
+fieldSummary <- function(x, groupBy, lengthColumn = "", na = "Unknown")
+{
+  #column=groupBy[1L];lengthColumn=lengthColumn2;na="Unknown"
+  if (nrow(x) == 0L) {
+    
+    message(
+      "The data frame given to fieldSummary() has no rows!"
+    )
+    
+    return(do.call(data.frame, args = c(as.list(x[groupBy]), list(
+      Count = integer(), Percentage = numeric()
+    ))))
+  }
+  
+  for (column in groupBy) {
+    
+    values <- kwb.utils::selectColumns(x, column)
+    levels(values) <- c(levels(values), na)
+    x[[column]] <- kwb.utils::defaultIfNA(values, na)
+  }
+  
+  y <- stats::aggregate(
+    kwb.utils::toFormula("Count", groupBy), 
+    data = cbind(x, Count = 1), 
+    FUN = length
+  )
+  
+  if (lengthColumn != "") {
+    
+    y1 <- stats::aggregate(
+      kwb.utils::toFormula(lengthColumn, groupBy), 
+      data = x, 
+      FUN = sum, 
+      na.rm = TRUE
+    )
+    
+    y <- merge(y, y1, by = groupBy)
+  }
+  
+  y$Percentage <- 100 * kwb.utils::selectColumns(y, "Count") / nrow(x)
+  
+  y
+}
