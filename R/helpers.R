51c488f9da0eb664a842c83a90ecbcefc2b7e785
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-01-23 20:50:46 +0100

    Add get_user_config_dir(), was missing!

--- R/helpers.R
+++ R/helpers.R
@@ -96,6 +96,12 @@ get_option <- function(
   value
 }
 
+# get_user_config_dir ----------------------------------------------------------
+get_user_config_dir <- function(default = NULL)
+{
+  kwb.utils::defaultIfNULL(get_global("user_config_dir"), default)
+}
+
 # in_development_mode ----------------------------------------------------------
 in_development_mode <- function()
 {
