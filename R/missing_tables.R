4e612c9259b0c49d451617f676231a49f6962b2f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-08-09 13:30:04 +0200

    Document contained_tables()

--- R/missing_tables.R
+++ R/missing_tables.R
@@ -30,6 +30,23 @@ remove_default <- function(config)
 }
 
 # contained_tables -------------------------------------------------------------
+
+#' Names of "Tables" Provided in Zip Archive
+#' 
+#' @param zip_file path to zip archive file (.zip or .7z)
+#' @param config csv configuration mapping table names to file names
+#' @return vector of character containing the names of the tables for which 
+#'   corresponding files have been found in the given zip archive
+#' @export
+#' @examples
+#' kwb.prep::contained_tables(
+#'   zip_file = kwb.prep:::invalid_zip("five-rows.zip"), 
+#'   config = list(
+#'     pipes = list(file = "Haltungen_SAN_utf8.csv"),
+#'     trees = list(file = "Haltungen_Baeume_utf8.csv"),
+#'     animals = list(file = "no-animals-there.csv")
+#'   )
+#' )
 contained_tables <- function(zip_file, config)
 {
   expected <- sapply(remove_default(config), kwb.utils::selectElements, "file")
